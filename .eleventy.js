2863093be02e95004d15ddf11e0b78d735a94105
Pear <20259871+TheRealPear@users.noreply.github.com>
2024-07-05 22:03:27 -0400

    Minify HTML in build & fix apple icon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const htmlmin = require("html-minifier-terser");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.setServerOptions({
+      showAllHosts: true,
+    });
+
     // Pass these files to compiled output
     eleventyConfig.addPassthroughCopy("./src/assets");
     // Copy favicon.ico to root directory for backward compatibility on some browsers
@@ -7,6 +13,21 @@ module.exports = function(eleventyConfig) {
     // Layout definitions
     eleventyConfig.addLayoutAlias("homepage", "_default/homepage.html");
     eleventyConfig.addLayoutAlias("base", "_default/base.html");
+
+    // Minify HTML files at build
+    eleventyConfig.addTransform("htmlmin", function (content) {
+      if ((this.page.outputPath || "").endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          collapseWhitespace: true,
+        });
+        return minified;
+      }
+      // If not an HTML output, return content as-is
+      return content;
+    });
 
     // Where 11ty should look for pages
     return {
